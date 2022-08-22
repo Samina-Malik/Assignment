72f6d02f56ade4bbac4640f07471f3cc923949b6
Samina Malik <samina.mmmalik@gmail.com>
2022-08-22 00:11:17 -0400

    2nd Commit

--- test/specs/home.js
+++ test/specs/home.js
@@ -7,17 +7,19 @@ describe("Browser Commands ", () => {
         
           await $('//input[@id="credentials-email"]').setValue('muhammad+test1@akira.md');
           await $('//input[@id="credentials-password"]').setValue('Tester123456#');
-          await $('//button[@id="credentials-submit"]').click().waitForDisplayed();
-          await $('')
+          await $('//button[@id="credentials-submit"]').click();
+                  
+        
+          
+          //Assert the URL_Text
+          await expect(browser).toHaveUrlContaining('home');
 
           //Assertion
           //Find Heading Element
           const headingE = await $('//header//h1');
-        
+
           //get Text
           const headingT = await headingE.getText();
-
-
           //Assert the Text
           await expect(headingT).toEqual('Welcome to TELUS Health Virtual Care');
   });
